cbc58bd0fc4d18143c830e7ee48f1d0825e31868
maxmillionsmax <maximilianocaetano@gmail.com>
2022-05-06 15:26:45 -0300

    Passando paramentros para o serviço RESTFULL

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,9 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init() {
-		return new ResponseEntity("Olá Usuário REST Spring Boot", HttpStatus.OK);
+	public ResponseEntity init(@RequestParam (value = "nome", required = true, defaultValue = "Nome não informado") String nome,
+			@RequestParam("salario") Long salario ) {
+		System.out.println("paramento sendo recebido " +nome );
+		return new ResponseEntity("Olá Usuário REST Spring Boot " +nome+", Salario é  "+ salario, HttpStatus.OK);
 	}
-	
 
 }
