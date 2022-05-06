0d4146f5d1d42c5c094287f6a7dec0b8ee0000df
maxmillionsmax <maximilianocaetano@gmail.com>
2022-05-06 18:31:38 -0300

    Criando serviço POST e efetuando cadastro de usuario

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,6 +48,22 @@ public class IndexController {
 	   List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
 		
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
+		
+	}
+	
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
+		
+	    Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+		
+	}
+	
+	@PostMapping(value = "/{iduser}/idvenda/{idvenda}", produces = "application/json")
+	public ResponseEntity cadastrarvenda(@PathVariable Long iduser, @PathVariable Long idvenda){
+			
+		return new ResponseEntity("id user: "+iduser+" idvenda "+idvenda, HttpStatus.OK);
 		
 	}
 
