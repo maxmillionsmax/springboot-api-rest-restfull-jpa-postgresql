5a254ab2e72116842750517efaef4ccb6f02cb3b
maxmillionsmax <maximilianocaetano@gmail.com>
2022-05-06 16:01:21 -0300

    Customizando metodos RESTFULL e URLs

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -20,13 +20,23 @@ public class IndexController {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping(value = "/{id}/codigovenda/{venda}", produces = "application/json")
+	public ResponseEntity<Usuario> relatorio(@PathVariable(value = "id")Long id
+			                                 ,@PathVariable(value = "venda")Long venda) {
+		
+	   	Optional<Usuario> usuario = usuarioRepository.findById(id);
+		
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+		
+	}
 
 	@GetMapping(value = "/{id}", produces = "application/json")
 	public ResponseEntity<Usuario> init(@PathVariable(value = "id")Long id) {
 		
 	   	Optional<Usuario> usuario = usuarioRepository.findById(id);
 		
-		return new ResponseEntity(usuario.get(), HttpStatus.OK);
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
 		
 	}
 	
