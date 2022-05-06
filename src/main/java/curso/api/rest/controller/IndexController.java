c3b27ca535081d8daef2e2e9c8339c308e0e3c84
maxmillionsmax <maximilianocaetano@gmail.com>
2022-05-06 15:55:41 -0300

    Criando serviço RESTFULL de consulta de usuario

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -1,41 +1,42 @@
 package curso.api.rest.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> init(@PathVariable(value = "id")Long id) {
+		
+	   	Optional<Usuario> usuario = usuarioRepository.findById(id);
+		
+		return new ResponseEntity(usuario.get(), HttpStatus.OK);
+		
+	}
+	
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> init() {
-
-		Usuario usuario = new Usuario();
-		usuario.setId(1L);
-		usuario.setLogin("admin");
-		usuario.setNome("Maximiliano");
-		usuario.setSenha("123");
+	public ResponseEntity<List<Usuario>> usuario(){
 		
-		Usuario usuario2 = new Usuario();
-		usuario2.setId(2L);
-		usuario2.setLogin("max");
-		usuario2.setNome("Nome");
-		usuario2.setSenha("max");
+	   List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
 		
-		List<Usuario> usuarios = new ArrayList<Usuario>();
-		usuarios.add(usuario);
-		usuarios.add(usuario2);
+		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 		
-		
-		return new ResponseEntity(usuarios, HttpStatus.OK);
 	}
 
 }
