f1fef3adfcce55a2e03612747e04941524a7f845
maxmillionsmax <maximilianocaetano@gmail.com>
2022-05-08 23:30:26 -0300

    JSON Web Token (JWT) - Criando a classe JWtApiAutenticacaoFilter

--- src/main/java/curso/api/rest/security/WebConfigSecurity.java
+++ src/main/java/curso/api/rest/security/WebConfigSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -34,8 +35,14 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 		.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
 		
 		/*Maperia URL de Logout e insvalida o usuário*/
-		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
-	
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		
+		/*Filtra requisições de login para autenticação*/
+		.and().addFilterBefore(new JWTLoginFilter("/login", authenticationManager()), 
+									UsernamePasswordAuthenticationFilter.class)
+		
+		/*Filtra demais requisições paa verificar a presenção do TOKEN JWT no HEADER HTTP*/
+		.addFilterBefore(new JwtApiAutenticacaoFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
 }
